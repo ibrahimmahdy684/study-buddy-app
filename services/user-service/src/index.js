43add33a7235100f85b03b3190f7bb7d920e94f5
ibrahimmahdy684 <ibrahim.elmahdy@Student.giu-uni.de>
2026-04-04 01:37:08 +0200

    project setup

---
+++ services/user-service/src/index.js
@@ -0,0 +1,16 @@
+import express from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("User Service Running");
+});
+
+const PORT = process.env.PORT || 4001;
+app.listen(PORT, () => {
+  console.log(`User Service running on port ${PORT}`);
+});
